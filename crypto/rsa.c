d5510b905a9af165e8797144c2c63629dffbf9f6
jmjatlanta <jmjatlanta@gmail.com>
2016-11-03 09:18:32 -0500

    Building a directory structure

---
+++ crypto/rsa.c
@@ -0,0 +1,9 @@
+//
+//  rsa.c
+//  c-libp2p
+//
+//  Created by John Jones on 11/3/16.
+//  Copyright © 2016 JMJAtlanta. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ test/testit.c
@@ -0,0 +1,17 @@
+
+int testit(const char* name, int (*func)(void)) {
+	printf("Testing %s...\n", name);
+	int retVal = func();
+	if (retVal)
+		printf("%s success!\n", name);
+	else
+		printf("** Uh oh! %s failed.**\n", name);
+	return retVal;
+}
+
+int main(int argc, char** argv) {
+	testit("test_repo_config_init", test_repo_config_init);
+	
+	return 1;
+}
+
