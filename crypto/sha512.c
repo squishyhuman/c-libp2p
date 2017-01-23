d3cfece2527042b6fbbb09c33d8936f54c9938b7
John Jones <jmjatlanta@gmail.com>
2017-01-23 11:31:24 -0500

    Added sha512 encoding

---
+++ crypto/sha512.c
@@ -0,0 +1,13 @@
+#include "mbedtls/sha512.h"
+
+/***
+ * hash a string using SHA512
+ * @param input the input string
+ * @param input_length the length of the input string
+ * @param output where to place the results
+ * @returns 1
+ */
+int libp2p_crypto_hashing_sha512(const unsigned char* input, size_t input_length, unsigned char output[128]) {
+	mbedtls_sha512(input, input_length, output, 0);
+	return 1;
+}
---
+++ include/libp2p/crypto/sha512.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/***
+ * hash a string using SHA512
+ * @param input the input string
+ * @param input_length the length of the input string
+ * @param output where to place the results
+ * @returns 1
+ */
+int libp2p_crypto_hashing_sha512(const unsigned char* input, size_t input_length, unsigned char output[128]);
+
